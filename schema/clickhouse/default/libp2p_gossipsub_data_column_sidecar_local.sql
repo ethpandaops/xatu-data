3767a67a5f08596905c5525eb92f737a317ed724
GitHub Action <action@github.com>
2026-04-07 18:30:24 +0000

    Update schema

--- schema/clickhouse/default/libp2p_gossipsub_data_column_sidecar_local.sql
+++ schema/clickhouse/default/libp2p_gossipsub_data_column_sidecar_local.sql
@@ -25,7 +25,7 @@ CREATE TABLE default.libp2p_gossipsub_data_column_sidecar_local
     `topic_fork_digest_value` LowCardinality(String) COMMENT 'Fork digest value of the topic',
     `topic_name` LowCardinality(String) COMMENT 'Name of the topic',
     `topic_encoding` LowCardinality(String) COMMENT 'Encoding used for the topic',
-    `meta_client_name` LowCardinality(String) COMMENT 'Name of the client that collected the data. The table contains data from multiple clients',
+    `meta_client_name` LowCardinality(String) COMMENT 'Name of the client that generated the event',
     `meta_client_id` String COMMENT 'Unique Session ID of the client that generated the event. This changes every time the client is restarted.' CODEC(ZSTD(1)),
     `meta_client_version` LowCardinality(String) COMMENT 'Version of the client that generated the event',
     `meta_client_implementation` LowCardinality(String) COMMENT 'Implementation of the client that generated the event',
@@ -46,4 +46,4 @@ ENGINE = ReplicatedReplacingMergeTree('/clickhouse/{installation}/{cluster}/defa
 PARTITION BY toYYYYMM(event_date_time)
 ORDER BY (slot_start_date_time, meta_network_name, meta_client_name, peer_id_unique_key, message_id)
 SETTINGS index_granularity = 8192
-COMMENT 'Contains data column sidecar messages received via libp2p gossipsub (PeerDAS). Collected from deep instrumentation within forked consensus layer clients. Each row represents a data column gossiped on the p2p network. Partition: monthly by `slot_start_date_time`.'
+COMMENT 'Table for libp2p gossipsub data column sidecar data'
