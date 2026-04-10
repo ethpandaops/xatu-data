d619946187862c7597074e66125af2f832dd53a2
GitHub Action <action@github.com>
2026-04-10 18:28:33 +0000

    Update schema

--- schema/clickhouse/default/canonical_beacon_proposer_duty_local.sql
+++ schema/clickhouse/default/canonical_beacon_proposer_duty_local.sql
@@ -7,7 +7,7 @@ CREATE TABLE default.canonical_beacon_proposer_duty_local
     `epoch_start_date_time` DateTime COMMENT 'The wall clock time when the epoch started' CODEC(DoubleDelta, ZSTD(1)),
     `proposer_validator_index` UInt32 COMMENT 'The validator index of the proposer for the slot' CODEC(ZSTD(1)),
     `proposer_pubkey` String COMMENT 'The public key of the validator proposer' CODEC(ZSTD(1)),
-    `meta_client_name` LowCardinality(String) COMMENT 'Name of the client that collected the data. The table contains data from multiple clients',
+    `meta_client_name` LowCardinality(String) COMMENT 'Name of the client that generated the event',
     `meta_client_id` String COMMENT 'Unique Session ID of the client that generated the event. This changes every time the client is restarted.' CODEC(ZSTD(1)),
     `meta_client_version` LowCardinality(String) COMMENT 'Version of the client that generated the event',
     `meta_client_implementation` LowCardinality(String) COMMENT 'Implementation of the client that generated the event',
@@ -34,4 +34,4 @@ ENGINE = ReplicatedReplacingMergeTree('/clickhouse/{installation}/{cluster}/defa
 PARTITION BY toStartOfMonth(slot_start_date_time)
 ORDER BY (slot_start_date_time, meta_network_name, proposer_validator_index, proposer_pubkey)
 SETTINGS index_granularity = 8192
-COMMENT 'Contains finalized proposer duty assignments. Each row represents which validator was scheduled to propose a block for a given slot. Partition: monthly by `slot_start_date_time`.'
+COMMENT 'Contains a proposer duty from a beacon block.'
